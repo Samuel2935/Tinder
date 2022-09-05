df88c75c6174f88e44be9f024d0807f686aeeb76
samuel2935 <samuelblessed38@gmail.com>
2022-09-05 05:38:03 -0700

    axios

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: "https://back-tin.herokuapp.com/"  //i repplaced my backend api with that of heroku server
+    baseURL: "https://back-tin.herokuapp.com"  //i repplaced my backend api with that of heroku server
 })
 export default instance;
